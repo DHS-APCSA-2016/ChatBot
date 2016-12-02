5dd66c8a733578d38003fc368f2d7665cbfe25e5
LeValleyC <levalley2329@mydusd.org>
2016-12-02 13:33:13 -0800

    Add files via upload

--- Sal9000Runner.java
+++ Sal9000Runner.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
-
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.Scanner;
+import java.io.*;
 public class Sal9000Runner
 {
     public static void main(String[] args)
@@ -9,11 +12,18 @@ public class Sal9000Runner
         Sal9000S sal2 = new Sal9000S();
         System.out.print('\u000C');
         System.out.println (sal1.getGreeting());
-        System.out.println ("Type salsoc to switch to Soccer department of Chatbot.");
+        System.out.println ("Type salsoc to switch to Mens Soccer department of Chatbot.");
         System.out.println ("Type salgen to switch to General department of Chatbot.");
-        System.out.println ("You can switch back and forth at any time.");
+        System.out.println ("You cannot switch modes once you have choosen (switch implement soon..)");
         Scanner in = new Scanner (System.in);
         String statement = in.nextLine();
+        String fileName = "out.txt";
+
+        while (!(statement.equals("salsoc") || statement.equals("salgen")))
+        {
+            System.out.println("Please select which chatbot mode to use!");
+            statement = in.nextLine();
+        }
         /** Switch statement to switch between departments of chatbot */
         switch (statement)
         {
@@ -21,7 +31,7 @@ public class Sal9000Runner
                 System.out.println("You are now being transferred to the Soccer Department.");
                 try 
                 {
-                    Thread.sleep(2500); // delays next action for 2.5 seconds
+                    Thread.sleep(1500); // delays next action for 1.5 seconds
                 } 
                 catch(InterruptedException ex) 
                 {
@@ -29,18 +39,41 @@ public class Sal9000Runner
                 }
                 System.out.println(sal2.hello());
                 statement = in.nextLine();
-                while (!(statement.equals("Bye") || statement.equals("bye")))
+                try {
+                    FileWriter fWriter = null;
+                    BufferedWriter writer = null;
+                    fWriter = new FileWriter("outputSoccer.txt");
+                    writer = new BufferedWriter(fWriter);
+                while (!(statement.equals("Bye") || statement.equals("bye") || statement.equals("salgen")))
                     {
-                        System.out.print('\u000C'); // clears screen
+                        System.out.print('\u000C');// clears screen
+                        System.out.println("///////////////////////////////////////////////////////\n" +
+                                           "//              Helpful Commands                     //\n" +
+                                           "// link      >>> DHS soccer website link             //\n" +
+                                           "// salsoc    >>> Switch to SoccerBot  (disabled)     //\n" +
+                                           "// salgen    >>> Switch to GeneralBot (disabled)     //\n" +
+                                           "// bye       >>> Exit chatbot                        //\n" +
+                                           "// help      >>> Basic question starters             //\n" +
+                                           "///////////////////////////////////////////////////////");
                         System.out.println (sal2.getResponse(statement));
+                            writer.write(statement);
+                            writer.newLine();                                        
+                            System.err.println("Your input of " + statement.length() + " characters was saved.");
                         statement = in.nextLine();
+                        }
+                        writer.close();
                     }
+                catch (Exception e) {
+                            System.out.println("Error!");
+                        }
+                System.out.println("An output.txt file has been created to log your inputs");
+                System.out.println("Goodbye!");
                 break;
             case "salgen" :
                 System.out.println("You are now being transferred to the General Department.");
                 try 
                 {
-                    Thread.sleep(2500); 
+                    Thread.sleep(1500); 
                 } 
                 catch(InterruptedException ex) 
                 {
@@ -48,12 +81,27 @@ public class Sal9000Runner
                 }
                 System.out.println(sal1.hello());
                 statement = in.nextLine();
+                try {
+                    FileWriter fWriter = null;
+                    BufferedWriter writer = null;
+                    fWriter = new FileWriter("outputGeneral.txt");
+                    writer = new BufferedWriter(fWriter);
                 while (!(statement.equals("Bye") || statement.equals("bye")))
                     {
                         System.out.print('\u000C');
-                        System.out.println (sal2.getResponse(statement));
+                        System.out.println (sal1.getResponse(statement));
+                        writer.write(statement);
+                        writer.newLine();                                        
+                        System.err.println("Your input of " + statement.length() + " characters was saved.");
                         statement = in.nextLine();
                     }
+                    writer.close();
+                }
+                catch (Exception e) {
+                            System.out.println("Error!");
+                        }
+                System.out.println("An output.txt file has been created to log your inputs");
+                System.out.println("Goodbye!");
                 break;
             default :
                 System.out.println("error"); /** I have not "stupid proofed" by code yet so it
@@ -61,3 +109,4 @@ public class Sal9000Runner
         }
     }
 }
+
